ef8925dd3431fe36ec34d136f468be3bc59be858
Jakub Konieczny <jakubx200@gmail.com>
2022-10-18 18:52:26 +0200

    Pierwszy program

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("Cześć! Mam na imię Kuba");
+        System.out.println(26);
+        System.out.println("K");
+    }
+}
