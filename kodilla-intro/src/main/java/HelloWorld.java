9ed7f49c2e7d44317bf7be4eff540fe215e45dd0
Jakub Konieczny <jakubx200@gmail.com>
2022-10-18 19:10:56 +0200

    Test - Zmiana z Kuby na Jakub

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
     public static void main (String[] args)
     {
         System.out.println("Hello World!");
-        System.out.println("Cześć! Mam na imię Kuba");
+        System.out.println("Cześć! Mam na imię Jakub");
         System.out.println(26);
         System.out.println("K");
     }
