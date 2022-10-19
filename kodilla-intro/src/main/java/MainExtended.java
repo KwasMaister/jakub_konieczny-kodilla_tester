5cee37c87faebe0b774276fb4ef3c3d4088048a4
Jakub Konieczny <jakubx200@gmail.com>
2022-10-19 18:52:45 +0200

    Zadanie: deklarowanie i wywoywanie zmiennych

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main (String[] args)
+    {
+        String myCar = "Opel Astra OPC";
+        char version = 'J';
+        int horsePower = 280;
+        String exampleText = "Średnie spalanie wynosi:";
+        double averageFuelConsumption = 10.5;
+        boolean goodCar = true;
+
+        System.out.println(myCar);
+        System.out.println("Moja wersja samochodu " + version);
+        System.out.println("Moc samochodu ma " + horsePower + " Koni mechanicznych");
+        System.out.println(exampleText);
+        System.out.println(averageFuelConsumption + " litra");
+        System.out.println("Czy jest to dobry samochód " + goodCar);
+    }
+}
