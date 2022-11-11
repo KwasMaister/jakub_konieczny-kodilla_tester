59ca582a616ad8ffb3fc0a40f01215b25d4f1d17
Jakub Konieczny <jakubx200@gmail.com>
2022-11-11 17:45:30 +0100

    Wczytanie ocen podawanych przez uzytkownika

--- kodilla-intro/src/main/java/GradesExcercise.java
+++ kodilla-intro/src/main/java/GradesExcercise.java
@@ -1,18 +1,15 @@
+import java.util.Scanner;
+
 public class GradesExcercise {
     public static void main (String [] args){
         Grades grades = new Grades();
-        grades.add(1);
-        grades.add(2);
-        grades.add(3);
-        grades.add(4);
-        grades.add(5);
-        grades.add(6);
-        grades.add(7);
-        grades.add(8);
-        grades.add(9);
-        grades.add(12);
-        grades.add(50);
-        grades.add(80);
+        System.out.println("Podaj 10 ocen");
+        Scanner ocena = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++){
+            int grade = ocena.nextInt();
+            grades.add(grade);
+        }
 
         System.out.println("Last Grades: " + grades.lastGrade());
         System.out.println("Average Grades: " + grades.averageGrades());
