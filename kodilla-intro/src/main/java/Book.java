edc654dd588ecfb4bb556bdf182ec62cd44e1a53
Jakub Konieczny <jakubx200@gmail.com>
2022-11-15 19:01:50 +0100

    Zadanie: Zmienne i metody statyczne - poprawka

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,26 +1,18 @@
-import java.util.Scanner;
+
 
 public class Book {
-   private static String author;
+    private static String author;
     private static String title;
 
-    public static String of (){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Podaj autora ksiazki");
-        author = scanner.nextLine().trim();
-
-        System.out.println("Podaj tytuł ksiazki");
-        title = scanner.nextLine().trim();
-
+    public static String of (String author, String title){
+        Book.author = author;
+        Book.title = title;
         return author + title;
-
     }
 
     public static void main (String [] args){
-
-        String book = Book.of();
-        System.out.println("Autor: " + author);
-        System.out.println("Tytuł: " + title);
+        Book.of("Kuba", "Przez swiat");
+        System.out.println("Ksiązka " + author + " " + title);
     }
+
 }
