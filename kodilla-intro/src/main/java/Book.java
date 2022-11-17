355afcbb9d7e93885daff6ab754494f3ce5ef9a4
Jakub Konieczny <jakubx200@gmail.com>
2022-11-17 20:46:31 +0100

    Przypisanie wartosci do obiektu typu Book

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -2,13 +2,17 @@ public class Book {
     private static String author;
     private static String title;
 
+    public Book(String author, String title) {
+    }
+
     public static Book of (String author, String title){
         Book.author = author;
         Book.title = title;
-        return new Book();
+        return new Book(author, title);
     }
 
     public static void main (String [] args){
+
        Book book = Book.of("Kuba", "Przez swiat");
         System.out.println("Ksiązka " + author + " " + title);
     }
