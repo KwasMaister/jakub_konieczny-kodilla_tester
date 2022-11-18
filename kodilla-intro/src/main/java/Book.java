ed867ffcf65b4998dd62e1033c0ee550b50d47f5
Jakub Konieczny <jakubx200@gmail.com>
2022-11-18 15:46:08 +0100

    warto jako nie statyczne ,Konstruktor przypisanie argumentow do zmiennych

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,19 +1,21 @@
 public class Book {
-    private static String author;
-    private static String title;
+    private String author;
+    private String title;
 
-    public Book(String author, String title) {
+    Book book = Book.of("Kuba", "Przez swiat");
+
+     public Book(String author, String title) {
+         this.author = author;
+         this.title = title;
     }
 
     public static Book of (String author, String title){
-        Book.author = author;
-        Book.title = title;
         return new Book(author, title);
     }
 
     public static void main (String [] args){
 
        Book book = Book.of("Kuba", "Przez swiat");
-        System.out.println("Ksiązka " + author + " " + title);
+        System.out.println("Ksiązka "+ book.title + book.title);
     }
 }
