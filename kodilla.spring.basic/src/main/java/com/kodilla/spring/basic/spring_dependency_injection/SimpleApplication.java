c57caf3b4aa203bd991ace4c9c51e99f91daa132
Jakub Konieczny <jakubx200@gmail.com>
2023-01-07 18:30:38 +0100

    9.1 --> wstydzikiwanie do pola

--- kodilla.spring.basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla.spring.basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -1,18 +1,19 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
     /*
     Beana możemy wstrzyknąć tylko do innego beana
+
+    autowired --> wstrzykiwanie do pola
+    Adnotacja @Autowired mówi: "znajdź beana o typie określonym poniżej i przypisz go do wskazanej zmiennej"
      */
 
 @Component
 public class SimpleApplication {
+    @Autowired
     private SkypeMessageService messageService;
-
-    public SimpleApplication(SkypeMessageService messageService) {
-        this.messageService = messageService;
-    }
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
